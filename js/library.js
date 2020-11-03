5b64df5a151680a72f4ff209be88ebdaa7bcc56c
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-11-03 18:30:05 +0800

    ADD js

--- js/library.js
+++ js/library.js
@@ -1,1 +1,19 @@
-console.log('hello')
+let myLibray = []
+
+function Book(author, title, num_pages, read = false) {
+  this.author = author
+  this.title = title
+  this.num_pages = num_pages
+  this.read = read ? 'read already.' : 'not read yet.'
+}
+
+addBookToLibrary = new Book()
+
+function addBookToLibrary(author, title, num_pages) {
+  let newBook = new Book(author, title, num_pages)
+  myLibray.push(newBook)
+}
+
+function removeBook() {
+
+}
